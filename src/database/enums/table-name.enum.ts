64b1f2c06f5d4b4a0a1937aabd4e1cb7acf9bd48
Valentun22 <666valentun666@gmail.com>
2025-01-03 16:52:08 +0200

    Initial commit

---
+++ src/database/enums/table-name.enum.ts
@@ -0,0 +1,11 @@
+export enum TableNameEnum {
+  FAVORITE = 'favorite',
+  MEETINGS = "meetings",
+  NEWS = "news",
+  REFRESH_TOKEN = 'refresh-tokens',
+  REVIEWS = "reviews",
+  USERS = 'users',
+  VENUES = "venues",
+  SIGNBOARD = "signboard",
+  STATISTICS = 'statistics',
+}
